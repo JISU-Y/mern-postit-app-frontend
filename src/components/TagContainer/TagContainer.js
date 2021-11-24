350d3f74c4fd54c391dd29341e6a38e1360d3480
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2021-11-25 00:34:08 +0900

    make TagContainer Component out of TodoList

---
+++ src/components/TagContainer/TagContainer.js
@@ -0,0 +1,45 @@
+import React from "react"
+import styled, { keyframes } from "styled-components"
+import Tag from "../Tag"
+
+const TagContainer = (props) => {
+  console.log(props)
+  return (
+    <div
+      className="tag-container"
+      style={{
+        pointerEvents: props.isEdit ? "initial" : "none",
+      }}
+    >
+      {/* editing warning */}
+      {props.isEdit ? (
+        <AnimatedComponent>
+          <div className="blinking-block">
+            <h5>EDITING!</h5>
+          </div>
+        </AnimatedComponent>
+      ) : null}
+      {props.tags.length > 0
+        ? props.tags.map((tag, index) => {
+            return <Tag key={index} tag={tag} handleRemoveTags={props.handleRemoveTags} />
+          })
+        : props.isEdit && <p className="no-tag">right click to add tags</p>}
+    </div>
+  )
+}
+
+// Blinking effect on edit / styled-components
+// => props 바깥에 해야 warning(The component styled.div with the id of  has been created dynamically.) 안 생김
+const blinkingEffect = () => {
+  return keyframes`
+        50% {
+          opacity: 0;
+        }
+      `
+}
+
+const AnimatedComponent = styled.div`
+  animation: ${blinkingEffect} 1s linear infinite;
+`
+
+export default TagContainer
