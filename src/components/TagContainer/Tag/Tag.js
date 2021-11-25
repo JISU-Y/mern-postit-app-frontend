6cff5cab22f934e880dfc020e7f582d8677ba6ba
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2021-11-25 23:26:27 +0900

    add TagContainer module css

--- src/components/TagContainer/Tag/Tag.js
+++ src/components/TagContainer/Tag/Tag.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { RiCloseCircleLine } from "react-icons/ri"
 
+import styles from "../TagContainer.module.css"
+
 const Tag = ({ tag, handleRemoveTags }) => {
   return (
-    <div className={`post-tag ${tag}`}>
-      <p>{tag}</p>
-      <RiCloseCircleLine className="rm-tag-btn btn" onClick={() => handleRemoveTags(tag)}>
-        X
-      </RiCloseCircleLine>
+    <div className={styles.tag}>
+      <p className={styles.content}>{tag}</p>
+      <RiCloseCircleLine className={styles.removeBtn} onClick={() => handleRemoveTags(tag)} />
     </div>
   )
 }
