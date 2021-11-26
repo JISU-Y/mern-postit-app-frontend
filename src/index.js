c38d9e8599fe909bca782f9a608ca38ccae8d140
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2021-11-26 10:15:25 +0900

    tag style in Tag component and postCSS nesting set

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux"; // state를 어디에서든 접근할 수 있도록 해주는 Provider
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux" // state를 어디에서든 접근할 수 있도록 해주는 Provider
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
 
-import { reducers } from "./reducers"; // reducer import
+import { reducers } from "./reducers" // reducer import
 
-import "./index.css";
-import App from "./App";
+import "./index.css"
+import App from "./App"
 
 // createStore (reducers / compose)
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
-);
+)
